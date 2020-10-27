08b7544fb0676b1579501c85be667adf8eb269c3
Asmei <kamsykula@gmail.com>
2020-10-27 23:41:20 +0100

    Added first coroutine type - FireAndForget class

--- include/tinycoro/FireAndForget.hpp
+++ include/tinycoro/FireAndForget.hpp
@@ -5,4 +5,50 @@
 #ifndef TINYCORO_FIREANDFORGET_HPP
 #define TINYCORO_FIREANDFORGET_HPP
 
+#include <coroutine>
+#include <type_traits>
+#include <exception>
+
+namespace tinycoro {
+    /*
+     * TODO prepare description
+     */
+    class FireAndForget : std::suspend_never {
+    public:
+        struct FireAndForgetPromise;
+
+        FireAndForget() = default;
+
+        FireAndForget(FireAndForget &) = delete;
+        FireAndForget &operator=(FireAndForget &) = delete;
+        FireAndForget(FireAndForget &&) = delete;
+        FireAndForget &operator=(FireAndForget &&) = delete;
+
+        auto operator
+        co_await () = delete;
+
+        using promise_type = FireAndForgetPromise;
+    };
+
+    struct FireAndForget::FireAndForgetPromise {
+        FireAndForget get_return_object() const noexcept {
+            return {};
+        }
+
+        void return_void() const noexcept {}
+
+        std::suspend_never initial_suspend() const noexcept {
+            return {};
+        }
+
+        std::suspend_never final_suspend() const noexcept {
+            return {};
+        }
+
+        void unhandled_exception() const noexcept {
+            std::terminate();
+        }
+    };
+}
+
 #endif //TINYCORO_FIREANDFORGET_HPP
--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,20 @@
 // Created by Asmei on 10/27/2020.
 //
 
+#include "tinycoro/FireAndForget.hpp"
+#include <coroutine>
+#include <iostream>
+
+tinycoro::FireAndForget test() {
+    co_await std::suspend_never{};
+    std::cout << "Hello" << std::endl;
+}
+
+tinycoro::FireAndForget test2() {
+    std::cout << "Hello" << std::endl;
+}
+
 int main() {
+    auto a = test2();
     return 0;
 }
