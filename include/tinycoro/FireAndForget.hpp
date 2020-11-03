64dd05d51adb9fd45a3b4c16d824966db72f0247
Asmei <kamsykula@gmail.com>
2020-11-03 23:58:07 +0100

    Adde clang-format file, reformat code styl of FireAndForget.hpp file

--- include/tinycoro/FireAndForget.hpp
+++ include/tinycoro/FireAndForget.hpp
@@ -6,49 +6,69 @@
 #define TINYCORO_FIREANDFORGET_HPP
 
 #include <coroutine>
-#include <type_traits>
 #include <exception>
+#include <type_traits>
 
-namespace tinycoro {
+namespace tinycoro
+{
     /*
      * TODO prepare description
      */
-    class FireAndForget : std::suspend_never {
+    class FireAndForget : std::suspend_never
+    {
     public:
         struct FireAndForgetPromise;
 
         FireAndForget() = default;
 
-        FireAndForget(FireAndForget &) = delete;
-        FireAndForget &operator=(FireAndForget &) = delete;
-        FireAndForget(FireAndForget &&) = delete;
-        FireAndForget &operator=(FireAndForget &&) = delete;
+        FireAndForget(FireAndForget&) = delete;
+        FireAndForget& operator=(FireAndForget&) = delete;
+        FireAndForget(FireAndForget&&) = delete;
+        FireAndForget& operator=(FireAndForget&&) = delete;
 
-        auto operator
-        co_await () = delete;
+        auto operator co_await() = delete;
 
         using promise_type = FireAndForgetPromise;
     };
 
-    struct FireAndForget::FireAndForgetPromise {
-        FireAndForget get_return_object() const noexcept {
+    struct FireAndForget::FireAndForgetPromise
+    {
+        FireAndForget get_return_object() const noexcept
+        {
             return {};
         }
 
-        void return_void() const noexcept {}
+        void return_void() const noexcept
+        {
+        }
 
-        std::suspend_never initial_suspend() const noexcept {
+        std::suspend_never initial_suspend() const noexcept
+        {
             return {};
         }
 
-        std::suspend_never final_suspend() const noexcept {
+        std::suspend_never final_suspend() const noexcept
+        {
             return {};
         }
 
-        void unhandled_exception() const noexcept {
+        void unhandled_exception() const noexcept
+        {
             std::terminate();
         }
     };
-}
 
-#endif //TINYCORO_FIREANDFORGET_HPP
+    template <typename T> FireAndForget fireAndForget(T t)
+    {
+        if constexpr(std::is_invocable_v<T>)
+        {
+            co_await t();
+        }
+        else
+        {
+            co_await t;
+        }
+    }
+} // namespace tinycoro
+
+#endif // TINYCORO_FIREANDFORGET_HPP
