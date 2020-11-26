4e858e9ccf77d28ec2faea5e59fa4919db8f04c4
Asmei <kamsykula@gmail.com>
2020-11-27 00:45:42 +0100

    Code format

--- include/tinycoro/Generator.hpp
+++ include/tinycoro/Generator.hpp
@@ -17,59 +17,8 @@ namespace tinycoro
         using promise_type = GeneratorPromiseType;
         using promise_coro_handle = std::coroutine_handle<promise_type>;
 
-        struct GeneratorPromiseType
-        {
-            std::variant<const T*, std::exception_ptr> value;
-
-            Generator get_return_object()
-            {
-                return Generator{promise_coro_handle::from_promise(*this)};
-            }
-
-            std::suspend_always initial_suspend()
-            {
-                return {};
-            }
-
-            std::suspend_always final_suspend()
-            {
-                return {};
-            }
-
-            std::suspend_always yield_value(const T& valRef)
-            {
-                this->value = std::addressof(valRef);
-                return {};
-            }
-
-            void return_void()
-            {}
-
-            template <typename Expression>
-            Expression&& await_transform(Expression&& expression)
-            {
-                static_assert(sizeof(expression) == 0, "co_await is not supported in coroutines of type generator");
-                return std::forward<Expression>(expression);
-            }
-
-            void unhandled_exception()
-            {
-                this->value = std::current_exception();
-            }
-
-            void rethrowExceptionIfExists()
-            {
-                if(auto exceptionPtr = std::get_if<std::exception_ptr>(&this->value))
-                {
-                    std::rethrow_exception(*exceptionPtr);
-                }
-            }
-        };
-
-
         Generator() = default;
-        Generator(GeneratorPromiseType& promise)
-            : coroHandle(promise_coro_handle::from_promise(promise))
+        Generator(promise_type& promise) : coroHandle(promise_coro_handle::from_promise(promise))
         {}
 
         ~Generator()
@@ -93,84 +42,141 @@ namespace tinycoro
             return *this;
         }
 
-
         Generator(const Generator&) = delete;
         Generator& operator=(const Generator&) = delete;
 
-        struct iterator
+        struct Iterator;
+
+        Iterator begin();
+        Iterator end();
+
+    private:
+        explicit Generator(promise_coro_handle coroutine) noexcept : coroHandle(coroutine)
+        {}
+        promise_coro_handle coroHandle = nullptr;
+    };
+
+    template <typename T>
+    struct Generator<T>::GeneratorPromiseType
+    {
+        std::variant<const T*, std::exception_ptr> value;
+
+        Generator<T> get_return_object()
         {
-            using iterator_category = std::input_iterator_tag;
-            using value_type = T;
-            using difference_type = ptrdiff_t;
-            using pointer = const T*;
-            using reference = const T&;
+            return Generator<T>{promise_coro_handle::from_promise(*this)};
+        }
 
-            promise_coro_handle coroHandle;
-
-            bool operator==(const iterator& other) const
-            {
-                return this->coroHandle == other;
-            }
-
-            bool operator!=(const iterator& other) const
-            {
-                return this->coroHandle != other.coroHandle;
-            }
-
-            iterator& operator++()
-            {
-                this->coroHandle.resume();
-
-                if(this->coroHandle.done())
-                {
-                    this->coroHandle = nullptr;
-                }
-
-                return *this;
-            }
-
-            iterator& operator++(int) = delete;
-
-            const T& operator*() const
-            {
-                return *std::get<const T*>(this->coroHandle.promise().value);
-            }
-
-            const T* operator->() const
-            {
-                return std::addressof(operator*());
-            }
-        };
-
-        iterator begin()
+        std::suspend_always initial_suspend()
         {
-            if(not this->coroHandle)
-            {
-                return iterator{nullptr};
-            }
+            return {};
+        }
 
+        std::suspend_always final_suspend()
+        {
+            return {};
+        }
+
+        std::suspend_always yield_value(const T& valRef)
+        {
+            this->value = std::addressof(valRef);
+            return {};
+        }
+
+        void return_void()
+        {}
+
+        template <typename Expression>
+        Expression&& await_transform(Expression&& expression)
+        {
+            static_assert(sizeof(expression) == 0, "co_await is not supported in coroutines of type generator");
+            return std::forward<Expression>(expression);
+        }
+
+        void unhandled_exception()
+        {
+            this->value = std::current_exception();
+        }
+
+        void rethrowExceptionIfExists()
+        {
+            if(auto exceptionPtr = std::get_if<std::exception_ptr>(&this->value))
+            {
+                std::rethrow_exception(*exceptionPtr);
+            }
+        }
+    };
+
+    template <typename T>
+    struct Generator<T>::Iterator
+    {
+        using iterator_category = std::input_iterator_tag;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = const T*;
+        using reference = const T&;
+
+        promise_coro_handle coroHandle;
+
+        bool operator==(const Iterator& other) const
+        {
+            return this->coroHandle == other;
+        }
+
+        bool operator!=(const Iterator& other) const
+        {
+            return this->coroHandle != other.coroHandle;
+        }
+
+        Iterator& operator++()
+        {
             this->coroHandle.resume();
 
             if(this->coroHandle.done())
             {
-                this->coroHandle.promise().rethrowExceptionIfExists();
-                return iterator{nullptr};
+                this->coroHandle = nullptr;
             }
 
-            return iterator{this->coroHandle};
+            return *this;
         }
 
-        iterator end()
+        Iterator& operator++(int) = delete;
+
+        const T& operator*() const
         {
-            return iterator{nullptr};
+            return *std::get<const T*>(this->coroHandle.promise().value);
         }
 
-    private:
-        explicit Generator(promise_coro_handle coroutine) noexcept
-        : coroHandle(coroutine)
-            {}
-        promise_coro_handle coroHandle = nullptr;
+        const T* operator->() const
+        {
+            return std::addressof(operator*());
+        }
     };
+
+    template <typename T>
+    typename Generator<T>::Iterator Generator<T>::begin()
+    {
+        if(not this->coroHandle)
+        {
+            return Iterator{nullptr};
+        }
+
+        this->coroHandle.resume();
+
+        if(this->coroHandle.done())
+        {
+            this->coroHandle.promise().rethrowExceptionIfExists();
+            return Iterator{nullptr};
+        }
+
+        return Iterator{this->coroHandle};
+    }
+
+    template <typename T>
+    typename Generator<T>::Iterator Generator<T>::end()
+    {
+        return Iterator{nullptr};
+    }
+
 } // namespace tinycoro
 
 #endif // TINYCORO_GENERATOR_HPP
