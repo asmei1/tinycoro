a00c298e6208e2b69d42c58e105f274fda1d5865
Asmei <kamsykula@gmail.com>
2020-12-02 23:00:21 +0100

    Replace using namespace with normal namespace

--- include/tinycoro/StaticCoroThreadPool.cpp
+++ include/tinycoro/StaticCoroThreadPool.cpp
@@ -4,88 +4,89 @@
 #include "StaticCoroThreadPool.hpp"
 #include <functional>
 
-using namespace tinycoro;
-
-StaticCoroThreadPool::StaticCoroThreadPool(size_t threadCount)
+namespace tinycoro
 {
-    for(int i = 0; i < threadCount; ++i)
+    StaticCoroThreadPool::StaticCoroThreadPool(size_t threadCount)
     {
-        this->workers.emplace_back([&](std::stop_token stopToken) { startWorker(stopToken); });
-    }
-}
-
-StaticCoroThreadPool::~StaticCoroThreadPool()
-{
-    for(auto& t : this->workers)
-    {
-        t.request_stop();
-    }
-
-    for(auto& t : this->workers)
-    {
-        t.join();
-    }
-}
-
-void StaticCoroThreadPool::startWorker(std::stop_token stopToken)
-{
-    ThreadPoolOperation* op;
-    while(!stopToken.stop_requested())
-    {
+        for(int i = 0; i < threadCount; ++i)
         {
-            std::unique_lock lock(this->operationsQueueMutex);
-            this->operationsCV.wait(lock, stopToken, [&]() { return not this->operationsQueue.empty(); });
+            this->workers.emplace_back([&](std::stop_token stopToken) { startWorker(stopToken); });
+        }
+    }
 
-            // If thread was stopped, stop worker too
-            if(stopToken.stop_requested())
-            {
-                return;
-            }
-            op = this->operationsQueue.front();
-            this->operationsQueue.pop();
+    StaticCoroThreadPool::~StaticCoroThreadPool()
+    {
+        for(auto& t : this->workers)
+        {
+            t.request_stop();
         }
 
-        // resume coroutine
-        op->awaitingCoro.resume();
-        this->waitForAllWorkersCV.notify_one();
+        for(auto& t : this->workers)
+        {
+            t.join();
+        }
     }
-}
 
-void StaticCoroThreadPool::scheduleOpOnPoolWhenIsSuspended(StaticCoroThreadPool::ThreadPoolOperation* operation)
-{
-    std::unique_lock lock(this->operationsQueueMutex);
+    void StaticCoroThreadPool::startWorker(std::stop_token stopToken)
+    {
+        ThreadPoolOperation* op;
+        while(!stopToken.stop_requested())
+        {
+            {
+                std::unique_lock lock(this->operationsQueueMutex);
+                this->operationsCV.wait(lock, stopToken, [&]() { return not this->operationsQueue.empty(); });
 
-    this->operationsQueue.push(operation);
-    this->operationsCV.notify_one();
-}
+                // If thread was stopped, stop worker too
+                if(stopToken.stop_requested())
+                {
+                    return;
+                }
+                op = this->operationsQueue.front();
+                this->operationsQueue.pop();
+            }
 
-void StaticCoroThreadPool::wait()
-{
-    std::unique_lock lock(this->operationsQueueMutex);
-    this->waitForAllWorkersCV.wait(lock, [&]() { return this->operationsQueue.empty(); });
-}
+            // resume coroutine
+            op->awaitingCoro.resume();
+            this->waitForAllWorkersCV.notify_one();
+        }
+    }
 
-size_t StaticCoroThreadPool::threadCount() const
-{
-    return this->workers.size();
-}
+    void StaticCoroThreadPool::scheduleOpOnPoolWhenIsSuspended(StaticCoroThreadPool::ThreadPoolOperation* operation)
+    {
+        std::unique_lock lock(this->operationsQueueMutex);
 
-StaticCoroThreadPool::Scheduler StaticCoroThreadPool::getScheduler()
-{
-    return StaticCoroThreadPool::Scheduler(*this);
-}
+        this->operationsQueue.push(operation);
+        this->operationsCV.notify_one();
+    }
 
-bool StaticCoroThreadPool::ThreadPoolOperation::await_ready() noexcept
-{
-    return false;
-}
+    void StaticCoroThreadPool::wait()
+    {
+        std::unique_lock lock(this->operationsQueueMutex);
+        this->waitForAllWorkersCV.wait(lock, [&]() { return this->operationsQueue.empty(); });
+    }
 
-void StaticCoroThreadPool::ThreadPoolOperation::await_suspend(std::coroutine_handle<> awaitingCoro)
-{
-    // Store given coro handle and put operation into
-    this->awaitingCoro = awaitingCoro;
-    this->pool.scheduleOpOnPoolWhenIsSuspended(this);
-}
+    size_t StaticCoroThreadPool::threadCount() const
+    {
+        return this->workers.size();
+    }
 
-void StaticCoroThreadPool::ThreadPoolOperation::await_resume() noexcept
-{}
+    StaticCoroThreadPool::Scheduler StaticCoroThreadPool::getScheduler()
+    {
+        return StaticCoroThreadPool::Scheduler(*this);
+    }
+
+    bool StaticCoroThreadPool::ThreadPoolOperation::await_ready() noexcept
+    {
+        return false;
+    }
+
+    void StaticCoroThreadPool::ThreadPoolOperation::await_suspend(std::coroutine_handle<> awaitingCoro)
+    {
+        // Store given coro handle and put operation into
+        this->awaitingCoro = awaitingCoro;
+        this->pool.scheduleOpOnPoolWhenIsSuspended(this);
+    }
+
+    void StaticCoroThreadPool::ThreadPoolOperation::await_resume() noexcept
+    {}
+} // namespace tinycoro
